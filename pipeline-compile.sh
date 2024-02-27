0e05b3e39b71d616c09adb95d2833c088806315a
rad1an <rad1an.pm@proton.me>
2024-02-27 11:47:04 +0200

    Fix optimization pipeline

--- pipeline-compile.sh
+++ pipeline-compile.sh
@@ -14,16 +14,16 @@ elif [ $# -eq 1 ]; then
     elif [ $1 = "-p" ]; then
         echo "===Running with Pipeline==="
         ../compile.sh -p 2>&1 >/dev/null | tee compile.log | grep --invert-match ".*\(Consider enabling PVS-Studio\|Sanitizers enabled\|[Ee]nabled in CMakeLists.txt\).*" | awk '!x[$0]++'
-    elif [ $1 = "-O" ]; then
+    elif [ $1 = "-o" ]; then
         echo "===Running with Optimization==="
-        ../compile.sh -O 2>&1 >/dev/null | tee compile.log | grep --invert-match ".*\(Consider enabling PVS-Studio\|Sanitizers enabled\|[Ee]nabled in CMakeLists.txt\).*" | awk '!x[$0]++'
+        ../compile.sh -o 2>&1 >/dev/null | tee compile.log | grep --invert-match ".*\(Consider enabling PVS-Studio\|Sanitizers enabled\|[Ee]nabled in CMakeLists.txt\).*" | awk '!x[$0]++'
     elif [ $1 = "-h" ]; then
         echo "Usage: pipeline-compile.sh [OPTION]"
         echo "Run compile.sh with -p option"
         echo "If no argument - run compile.sh"
         echo "If --v='<value>' - run compile.sh with --v='<value>' argument - run Valgrind afterwards with <value> as arguments"
         echo "If -p argument - run compile.sh with -p argument - do pipeline of different compilers and sanitizers"
-        echo "If -O argument - run compile.sh with -O argument - do compilation with optimization"
+        echo "If -o argument - run compile.sh with -o argument - do compilation with optimization"
         echo "If --r='<value>' argument - run the value as a bash command instead of compilation"
         echo "If -c argument - clean cmake-build-* directories and compile.log"
         echo "If -h argument - print help"
